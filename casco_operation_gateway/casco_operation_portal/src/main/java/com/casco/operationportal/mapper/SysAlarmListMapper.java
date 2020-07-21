fbff37ee82de49226a8817f4d7d9206d45c26496
zxing <2500267824@qq.com>
2020-07-21 17:00:38 +0800

    多专业新增告警列表查看 前后端更新

---
+++ casco_operation_gateway/casco_operation_portal/src/main/java/com/casco/operationportal/mapper/SysAlarmListMapper.java
@@ -0,0 +1,14 @@
+package com.casco.operationportal.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.casco.operationportal.entity.SysAlarmList;
+
+/**
+ * @program: xiot_platform
+ * @description:
+ * @author: CC
+ * @date: 2020/7/21 4:19 PM
+ */
+public interface SysAlarmListMapper extends BaseMapper<SysAlarmList> {
+
+}
---
+++ casco_operation_gateway/casco_operation_portal/src/main/java/com/casco/operationportal/service/impl/SysAlarmListServiceImpl.java
@@ -0,0 +1,18 @@
+package com.casco.operationportal.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.casco.operationportal.entity.SysAlarmList;
+import com.casco.operationportal.mapper.SysAlarmListMapper;
+import com.casco.operationportal.service.SysAlarmListService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: xiot_platform
+ * @description:
+ * @author: CC
+ * @date: 2020/7/21 4:18 PM
+ */
+@Service
+public class SysAlarmListServiceImpl extends ServiceImpl<SysAlarmListMapper, SysAlarmList> implements SysAlarmListService {
+
+}
