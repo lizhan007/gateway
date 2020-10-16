af4ca511d0b789d98a7943f86680c7ebf2777bdd
36458 <0603521azyf>
2020-10-16 19:11:38 +0800

    屏蔽门接入

--- casco_operation_gateway/casco_opgw_screen_door/src/main/java/com/casco/opgw/screendoor/kafka/impl/KafkaServiceImpl.java
+++ casco_operation_gateway/casco_opgw_screen_door/src/main/java/com/casco/opgw/screendoor/kafka/impl/KafkaServiceImpl.java
@@ -1,10 +1,14 @@
 package com.casco.opgw.screendoor.kafka.impl;
 
 import com.casco.opgw.screendoor.kafka.KafkaService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
 public class KafkaServiceImpl implements KafkaService {
 
     @Value("${kafka.casco_opgw_signal_analog_topic}")
